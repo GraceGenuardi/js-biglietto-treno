88dd5859869ac89cf5c6e2a3c74bfdfcc93c21f4
GraceGenuardi <graziagenuardi96@gmail.com>
2023-01-31 14:53:10 +0100

    EDIT JS-aggiunto prompt

--- js/app.js
+++ js/app.js
@@ -1,12 +1,11 @@
 
 
-
-
+/*1-Chiedi all'utente la distanza */
 const distance = parseFloat(prompt("Quanti km vuoi percorrere?"));
+
+/*2-Chiedi all'utente l'età */
 const age = parseInt(prompt("Qual è la tua età?"));
 
-/*Let km: calcola la distanza*/
-let euro_km  = 0.21;
 
 
 
