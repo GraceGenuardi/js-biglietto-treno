79c173453d9f752e4b046537734db13a481949e1
GraceGenuardi <graziagenuardi96@gmail.com>
2023-01-31 14:52:24 +0100

    EDIT JS-Aggiunti PROMPT

--- js/app.js
+++ js/app.js
@@ -1,4 +1,15 @@
-console.log('prendo elemento html con id first-title')
-const titleElement = document.getElementById('first-title')
+
+
+
+
+const distance = parseFloat(prompt("Quanti km vuoi percorrere?"));
+const age = parseInt(prompt("Qual è la tua età?"));
+
+/*Let km: calcola la distanza*/
+let euro_km  = 0.21;
+
+
+
+
 
 
