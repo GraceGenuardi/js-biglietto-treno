8b69a1a81e9dd35c2682e6d28cc3782f4c68f2f8
GraceGenuardi <graziagenuardi96@gmail.com>
2023-01-31 15:10:50 +0100

    EDIT JS stabilita operazione di calcolo  per il prezzo del biglietto

--- js/app.js
+++ js/app.js
@@ -6,11 +6,19 @@ const distance = parseFloat(prompt("Quanti km vuoi percorrere?"));
 /*2-Chiedi all'utente l'età */
 const age = parseInt(prompt("Qual è la tua età?"));
 
+
 /*3-Calcolo costo biglietto per km e percentuale di sconto in base età */
 
-const ticketPerKm = 0.21; /*Costo biglietto per km*/
+const bigliettoPerKm = 0.21; /*Costo biglietto per km*/
 const scontoTeeneger = 0.2; /*Valore sconto under 18*/
 const scontoAnziani = 0.4;  /*Valore sconto over 65*/
+
+
+/*4- Stabilisci l'operazione per cui dare un prezzo al biglietto in base ai km e al costo del biglietto per km*/
+
+let bigliettoPrezzo = distance * bigliettoPerKm;
+
+
 
 
 
